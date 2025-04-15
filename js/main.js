114759dfce8c84147f02c7d496e7ec98b1e5f02a
jbmunoz <sineaxis@gmail.com>
2025-04-15 12:03:11 +0200

    Commit inicial

---
+++ js/main.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const btnMenu = document.querySelector('#btn-menu');
+    
+    btnMenu.addEventListener('click', (e) => {
+        const menu = document.querySelector('#menu');
+        menu.classList.toggle('close');        
+    });
+})
