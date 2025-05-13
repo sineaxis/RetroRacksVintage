fa8dff899e5252db6b454dbc1a9705a44ae544a3
jbmunoz <sineaxis@gmail.com>
2025-05-13 09:04:29 +0200

    Añadida funcionalidad de filtros al js

--- js/main.js
+++ js/main.js
@@ -5,4 +5,25 @@ document.addEventListener("DOMContentLoaded", () => {
     const menu = document.querySelector("#menu");
     menu.classList.toggle("close");
   });
+
+  function getFilterFromURL() {
+    const params = new URLSearchParams(window.location.search);
+    return params.get("filter");
+  }
+
+  function showFilteredProducts(filter) {
+    const products = document.querySelectorAll(".product");
+    products.forEach((product) => {
+      const category = product.dataset.category;
+      if (!filter || category === filter) {
+        product.style.display = "block";
+      }
+    });
+  }
+
+  const params = new URLSearchParams(window.location.search);
+  if (params.has("filter")) {
+    const filter = getFilterFromURL();
+    showFilteredProducts(filter);
+  }
 });
